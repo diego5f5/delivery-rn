a1e9ad58ceae1b0e95946832989b4d3dbc038225
Diego Ferreira <diegoferreira777@hotmail.com>
2022-07-25 11:44:40 -0300

    basket screen layout and currency presentation

--- src/screens/BasketScreen.js
+++ src/screens/BasketScreen.js
@@ -4,22 +4,31 @@ import {
   SafeAreaView,
   TouchableOpacity,
   Image,
+  ScrollView,
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { XCircleIcon } from "react-native-heroicons/solid";
+import Currency from "react-currency-formatter";
 
 import { useDispatch, useSelector } from "react-redux";
 import { selectRestaurant } from "../store/features/restaurantSlice";
-import { selectBasketItems } from "../store/features/basketSlice";
+import {
+  selectBasketItems,
+  removeFromBasket,
+  selectBasketTotal,
+} from "../store/features/basketSlice";
 
 const BasketScreen = () => {
   const navigation = useNavigation();
   const restaurant = useSelector(selectRestaurant);
   const items = useSelector(selectBasketItems);
+  const basketTotal = useSelector(selectBasketTotal);
   const dispatch = useDispatch();
 
   const [groupedItemsInBasket, setGroupedItemsInBasket] = useState();
+
+  const deliveryFee = 5.99;
 
   useEffect(() => {
     const groupedItems = items.reduce((results, item) => {
@@ -51,8 +60,74 @@ const BasketScreen = () => {
           </TouchableOpacity>
         </View>
 
-        <View>
-          <Image source={{ uri: restaurant.imageUrl }} />
+        <View className="flex-row items-center space-x-4 px-4 py-3 bg-white my-5">
+          <Image
+            source={{ uri: restaurant.imgUrl }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+          <Text className="flex-1">Deliver in 50-75 min</Text>
+          <TouchableOpacity>
+            <Text className="text-[#00CCBB]">Change</Text>
+          </TouchableOpacity>
+        </View>
+
+        <ScrollView className="divide-y divide-gray-200">
+          {groupedItemsInBasket &&
+            Object.entries(groupedItemsInBasket).map(([key, items]) => (
+              <View
+                key={key}
+                className="flex-row items-center space-x-3 bg-white py-2 px-5"
+              >
+                <Text className="text-[#00CCBB]">{items.length} x</Text>
+                <Image
+                  source={{ uri: items[0]?.image }}
+                  className="h-12 w-12 rounded-full"
+                />
+                <Text className="flex-1">{items[0]?.name}</Text>
+
+                <Text className="text-gray-600">
+                  <Currency quantity={items[0]?.price} currency="GBP" />
+                </Text>
+
+                <TouchableOpacity>
+                  <Text
+                    className="text-[#00CCBB] text-xs"
+                    onPress={() => dispatch(removeFromBasket({ id: key }))}
+                  >
+                    Remove
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            ))}
+        </ScrollView>
+
+        <View className="p-5 bg-white mt-5 space-y-4">
+          <View className="flex-row justify-between">
+            <Text className="text-gray-400">Subtotal</Text>
+            <Text className="text-gray-400">
+              <Currency quantity={basketTotal} currency="GBP" />
+            </Text>
+          </View>
+
+          <View className="flex-row justify-between">
+            <Text className="text-gray-400">Delivery Fee</Text>
+            <Text className="text-gray-400">
+              <Currency quantity={deliveryFee} currency="GBP" />
+            </Text>
+          </View>
+
+          <View className="flex-row justify-between">
+            <Text>Order Total</Text>
+            <Text className="font-extrabold">
+              <Currency quantity={basketTotal + deliveryFee} currency="GBP" />
+            </Text>
+          </View>
+
+          <TouchableOpacity className="rounded-lg bg-[#00CCBB] p-4">
+            <Text className="text-center text-white text-lg font-bold">
+              Place Order
+            </Text>
+          </TouchableOpacity>
         </View>
       </View>
     </SafeAreaView>
